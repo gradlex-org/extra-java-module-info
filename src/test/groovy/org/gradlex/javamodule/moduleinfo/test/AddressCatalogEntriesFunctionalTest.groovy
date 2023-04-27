d16447309469e749ade33c565ee17c89a5ec6edb
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-04-27 12:36:36 +0200

    Fix issue in test setup

--- src/test/groovy/org/gradlex/javamodule/moduleinfo/test/AddressCatalogEntriesFunctionalTest.groovy
+++ src/test/groovy/org/gradlex/javamodule/moduleinfo/test/AddressCatalogEntriesFunctionalTest.groovy
@@ -4,7 +4,7 @@ import org.gradlex.javamodule.moduleinfo.test.fixture.GradleBuild
 import org.gradlex.javamodule.moduleinfo.test.fixture.LegacyLibraries
 import spock.lang.IgnoreIf
 
-@IgnoreIf({ GradleBuild.gradleVersionUnderTest.startsWith("6.") })
+@IgnoreIf({ GradleBuild.gradleVersionUnderTest?.startsWith("6.") })
 class AddressCatalogEntriesFunctionalTest extends AbstractFunctionalTest {
 
     LegacyLibraries libs = new LegacyLibraries(false, true)
