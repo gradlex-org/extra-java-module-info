59e3122ba078c91cbb7c80829d9c2b959c13a0bb
Jendrik Johannes <jendrik@onepiece.software>
2026-01-30 13:32:01 +0100

    Update org.gradlex.internal-build-conventions to v0.10

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
-plugins { id("org.gradlex.internal-build-conventions") version "0.9" }
+plugins { id("org.gradlex.internal-build-conventions") version "0.10" }
 
 rootProject.name = "extra-java-module-info"
