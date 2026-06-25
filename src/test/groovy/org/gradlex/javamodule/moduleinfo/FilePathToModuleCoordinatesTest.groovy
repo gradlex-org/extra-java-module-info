23b06a73951d6f4512aab87a049cfc0ab053b477
Jendrik Johannes <jendrik.johannes@gmail.com>
2026-06-25 13:59:36 +0200

    Handle classifiers in FilePathToModuleCoordinates (#249)

--- src/test/groovy/org/gradlex/javamodule/moduleinfo/FilePathToModuleCoordinatesTest.groovy
+++ src/test/groovy/org/gradlex/javamodule/moduleinfo/FilePathToModuleCoordinatesTest.groovy
@@ -30,7 +30,7 @@ class FilePathToModuleCoordinatesTest extends Specification {
         def path = path('/Users/someone/.gradle/caches/modules-2/files-2.1/org.slf4j/slf4j-api/1.7.36/6c62681a2f655b49963a5983b8b0950a6120ae14/slf4j-api-1.7.36.jar')
 
         expect:
-        gaCoordinatesFromFilePathMatch(path, "org.slf4j:slf4j-api")
+        gaCoordinatesFromFilePathMatch(path, "org.slf4j:slf4j-api", null)
     }
 
     def "ga coordinates from gradle cache file path (version in file name does not match)"() {
@@ -38,7 +38,16 @@ class FilePathToModuleCoordinatesTest extends Specification {
         def path = path('/Users/jendrik/.gradle/caches/modules-2/files-2.1/com.google.guava/guava/33.2.1-jre/818e780da2c66c63bbb6480fef1f3855eeafa3e4/guava-33.2.1-android.jar')
 
         expect:
-        gaCoordinatesFromFilePathMatch(path, "com.google.guava:guava")
+        gaCoordinatesFromFilePathMatch(path, "com.google.guava:guava", null)
+    }
+
+    def "ga coordinates from gradle cache file path with classifier"() {
+        given:
+        def path = path('/Users/jendrik/.gradle/caches/modules-2/files-2.1/org.lwjgl/lwjgl-glfw/3.3.6/399b42b491c5dfa6595ae6fd79dcba61b93538a7/lwjgl-glfw-3.3.6-natives-macos-arm64.jar')
+
+        expect:
+        !gaCoordinatesFromFilePathMatch(path, "org.lwjgl:lwjgl-glfw", null)
+        gaCoordinatesFromFilePathMatch(path, "org.lwjgl:lwjgl-glfw", "natives-macos-arm64")
     }
 
     def "version from m2 repo file path"() {
@@ -64,13 +73,13 @@ class FilePathToModuleCoordinatesTest extends Specification {
         jarPath = path('/Users/someone/.m2/repository/com/google/code/findbugs/jsr305/3.0.2/jsr305-3.0.2.jar')
 
         then:
-        gaCoordinatesFromFilePathMatch(jarPath, "com.google.code.findbugs:jsr305")
+        gaCoordinatesFromFilePathMatch(jarPath, "com.google.code.findbugs:jsr305", null)
 
         when:
         jarPath = path('/Users/someone/.m2/repository/de/odysseus/juel/juel-impl/2.2.7/juel-impl-2.2.7.jar')
 
         then:
-        gaCoordinatesFromFilePathMatch(jarPath, "de.odysseus.juel:juel-impl")
+        gaCoordinatesFromFilePathMatch(jarPath, "de.odysseus.juel:juel-impl", null)
     }
 
     def "ga coordinates from m2 repo file path (version in file name does not match)"() {
@@ -78,7 +87,16 @@ class FilePathToModuleCoordinatesTest extends Specification {
         def path = path('/Users/someone/.m2/repository/com/google/guava/guava/33.2.1-jre/guava-33.2.1-android.jar')
 
         expect:
-        gaCoordinatesFromFilePathMatch(path, "com.google.guava:guava")
+        gaCoordinatesFromFilePathMatch(path, "com.google.guava:guava", null)
+    }
+
+    def "ga coordinates from m2 repo file path with classifier"() {
+        given:
+        def path = path('/Users/someone/.m2/repository/org/lwjgl/lwjgl-glfw/3.3.6/lwjgl-glfw-3.3.6-natives-macos-arm64.jar')
+
+        expect:
+        !gaCoordinatesFromFilePathMatch(path, "org.lwjgl:lwjgl-glfw", null)
+        gaCoordinatesFromFilePathMatch(path, "org.lwjgl:lwjgl-glfw", "natives-macos-arm64")
     }
 
     private Path path(String path) {
